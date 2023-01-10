36e13356e50b90e5d6952878658a9c5211a1581b
Nik Kolesnikov <niknills94@gmail.com>
2023-01-10 10:58:46 +0300

    refactor: changed the solution to a more concise one

--- src/index.js
+++ src/index.js
@@ -39,43 +39,37 @@ const MORSE_TABLE = {
 
 function decode(expr) {
   const arr = [];
-  let str = "";
-  let count = 0;
 
-  for (let char of expr) {
-    str += char;
-    count++;
+  for (let start = 0; start < expr.length; start += 10) {
+    let end = start + 10;
 
-    if (count === 10) {
-      arr.push(str);
-      count = 0;
-      str = "";
-    }
+    arr.push(expr.slice(start, end));
   }
 
-  return arr
-    .map((item) => {
-      let letter = "";
+  const morseArr = arr.map((item) => {
+    let letter = "";
 
-      if (item === "**********") {
-        return (letter = " ");
+    if (item === "**********") {
+      return (letter = " ");
+    }
+
+    for (let i = 0; i < item.length; i += 2) {
+      if (item[i] === "1" && item[i + 1] === "0") {
+        letter += ".";
+      } else if (item[i] === "1" && item[i + 1] === "1") {
+        letter += "-";
       }
+    }
 
-      for (let i = 0; i <= item.length; i += 2) {
-        if (item[i] === "1" && item[i + 1] === "1") {
-          letter += "-";
-        } else if (item[i] === "1" && item[i + 1] === "0") {
-          letter += ".";
-        }
-      }
+    return letter;
+  });
 
-      return letter;
-    })
-    .map((letter) => {
-      if (letter === " ") {
+  return morseArr
+    .map((i) => {
+      if (i in MORSE_TABLE) {
+        return MORSE_TABLE[i];
+      } else {
         return " ";
-      } else if (letter in MORSE_TABLE) {
-        return MORSE_TABLE[letter];
       }
     })
     .join("");
